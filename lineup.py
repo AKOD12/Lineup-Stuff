a99181b41e75176c7bac6ed1b64122ebca89558d
Ankith Kodali <ankith.kodali@gmail.com>
2024-01-23 11:01:12 -0500

    for the internet

--- lineup.py
+++ lineup.py
@@ -3,9 +3,20 @@ import pandas as pd
 import os
 import re
 
-# Helper functions for specific calculations
+# functions for specific calculations
 def count_shots(result):
     return 1 if result in ['O2', 'O3', 'X2', 'X3', 'X3F', 'X2F', 'TO'] else 0
+
+def style_plus_minus(val):
+    """
+    Returns the style configuration for the '+/-' column based on the value.
+    """
+    if val > 0:
+        return {'backgroundColor': 'green', 'color': 'white'}
+    elif val < 0:
+        return {'backgroundColor': 'red', 'color': 'white'}
+    else:
+        return {}
 
 def calculate_points(result):
     if result in ['O3', 'O3F']:
@@ -55,14 +66,12 @@ def calculate_plus_minus(offensive_stats, defensive_stats):
 
 
 def count_rebounds(df, column, term):
-    # Count occurrences of the specified term in the specified column
     return df[column].apply(lambda x: x.split(',').count(term) if isinstance(x, str) else 0)
 
 
 def process_stats(df, offense_or_defense):
     filtered_df = df[df['Row'] == offense_or_defense]
 
-    # Count rebounds first
     if offense_or_defense == 'OFFENSE':
         rebound_term = 'GT Off Reb'
         rebound_column_name = 'GT Offensive Rebounds'
@@ -72,7 +81,6 @@ def process_stats(df, offense_or_defense):
         rebound_column_name = 'Opponent Offensive Rebounds'
         filtered_df[rebound_column_name] = count_rebounds(filtered_df, 'DEFENSE', rebound_term)
 
-    # Then calculate other stats
     filtered_df['Points'] = filtered_df['Result'].apply(calculate_points)
     filtered_df['Shots'] = filtered_df['Result'].apply(count_shots)
     filtered_df['Turnovers'] = filtered_df['Result'].apply(count_turnovers)
@@ -83,10 +91,10 @@ def process_stats(df, offense_or_defense):
     filtered_df['FT Made'] = filtered_df['Result'].apply(count_free_throws_made)
     filtered_df['FT Attempted'] = filtered_df['Result'].apply(count_free_throws_attempted)
 
-    # Adjust possessions calculation by subtracting rebounds
+    # adjust possessions calculation by subtracting rebounds
     filtered_df['Possessions'] = filtered_df['Shots'] + filtered_df['Turnovers'] - filtered_df[rebound_column_name]
 
-    # Define aggregation functions
+    # aggregation functions
     agg_functions = {
         'Points': 'sum',
         'Shots': 'sum',
@@ -130,11 +138,9 @@ def load_and_process_data(selected_files):
         df = pd.read_csv(file_path)
         df['ON COURT'] = df['ON COURT'].apply(sort_cell_contents)
 
-        # Check for the existence of 'HALF COURT OFFENSE' and 'HALF COURT ACTIONS'
         half_court_offense = 'HALF COURT OFFENSE' if 'HALF COURT OFFENSE' in df.columns else ''
         half_court_actions = 'HALF COURT ACTIONS' if 'HALF COURT ACTIONS' in df.columns else ''
 
-        # Combine the two columns if they exist, handle cases where one might not exist
         if half_court_offense and half_court_actions:
             df['Half Court Combined'] = df[half_court_offense].fillna('') + ',' + df[half_court_actions].fillna('')
         elif half_court_offense:
@@ -151,19 +157,19 @@ def load_and_process_data(selected_files):
     return combined_df
 
 
-# Streamlit app layout
+# streamlit app layout
 st.markdown("Created by: Ankith Kodali (more commonly known as AK)")
 
-# Layout for GT logo and title
+# logo
 col1, col2 = st.columns([1, 4])
 
 with col1:
     st.image('gtlogo.svg', width=120)
 
 with col2:
-    st.title('Lineup Analysis')  # App title
+    st.title('Lineup Analysis')
 
-# Automatic listing of files in 'game-csv-2023' folder
+# listing of files in 'game-csv-2023' folder
 try:
     files_with_extension = [f for f in os.listdir('game-csv-2023') if f.endswith('.csv')]
     files = [f[:-4] for f in files_with_extension]  # Remove the '.csv' from each filename
@@ -172,7 +178,7 @@ try:
 except Exception as e:
     st.error(f'Error accessing folder: {e}')
 
-# Load data button
+# load data button
 if st.button('Load Data'):
     if selected_files:
         try:
@@ -185,30 +191,46 @@ if st.button('Load Data'):
     else:
         st.error('No files selected. Please select files for analysis.')
 
-# Filtering options for Half Court Offense and Actions
+# filtering options
 if 'data_loaded' in st.session_state and st.session_state['data_loaded']:
-    # Filtering for Half Court Combined Actions
+    # filter for half court actions
     combined_actions = st.session_state['combined_df']['Half Court Combined'].dropna().unique()
     excluded_options = ['', 'NaN', 'ISO, DK/MVMT']
     combined_actions_options = [action for action in combined_actions if action not in excluded_options]
     selected_combined_actions = st.multiselect('Filter by Combined Half Court Actions:', combined_actions_options)
 
-    # Transition filter
+    # transition filter
     transition_options = st.session_state['combined_df']['TRANSITION'].dropna().unique()
     selected_transition = st.multiselect('Filter by Transition:', transition_options)
 
-    # Apply filters to the DataFrame
+    # filter df
     filtered_df = st.session_state['combined_df']
     if selected_combined_actions:
         filtered_df = filtered_df[filtered_df['Half Court Combined'].isin(selected_combined_actions)]
     if selected_transition:
         filtered_df = filtered_df[filtered_df['TRANSITION'].isin(selected_transition)]
 
-    # Process the filtered data
+    # filtered data
     offensive_stats = process_stats(filtered_df, 'OFFENSE')
     defensive_stats = process_stats(filtered_df, 'DEFENSE')
+    # +/- calc
+    plus_minus_stats = offensive_stats[['ON COURT', 'Points']].merge(
+        defensive_stats[['ON COURT', 'Points']], on='ON COURT', how='left', suffixes=('_Off', '_Def')
+    )
+    plus_minus_stats['+/-'] = plus_minus_stats['Points_Off'] - plus_minus_stats['Points_Def']
 
-    # Display data based on user input
+    # combine +/- with offense and defense stats
+    offensive_stats = offensive_stats.merge(plus_minus_stats[['ON COURT', '+/-']], on='ON COURT')
+    offensive_stats = offensive_stats[['ON COURT', '+/-'] + [col for col in offensive_stats.columns if col not in ['ON COURT', '+/-']]]
+    defensive_stats = defensive_stats.merge(plus_minus_stats[['ON COURT', '+/-']], on='ON COURT')
+    defensive_stats = defensive_stats[['ON COURT', '+/-'] + [col for col in defensive_stats.columns if col not in ['ON COURT', '+/-']]]
+
+    # html for maybe color code
+    offensive_html = offensive_stats.to_html(escape=False)
+    defensive_html = defensive_stats.to_html(escape=False)
+
+
+    # data based on user input
     min_poss = st.number_input('Enter minimum possessions for lineups to display:', min_value=1, value=5)
     view_mode = st.radio("View Mode", ('Offensive Stats', 'Defensive Stats'))
     if view_mode == 'Offensive Stats':
@@ -216,6 +238,7 @@ if 'data_loaded' in st.session_state and st.session_state['data_loaded']:
         sorted_data = filtered_data.sort_values(by='Possessions', ascending=False)
         sorted_data.index = range(1, len(sorted_data) + 1)
         st.dataframe(sorted_data)
+
     elif view_mode == 'Defensive Stats':
         filtered_data = defensive_stats[defensive_stats['Possessions'] >= min_poss]
         sorted_data = filtered_data.sort_values(by='Possessions', ascending=False)
